8859ac7acb51e2c86d82505cfdd89346d5ec68cd
franca <francavisokolskis@gmail.com>
2022-10-07 14:20:15 -0300

    footer pinned down

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
+import { Stack, Box } from "@mui/system";
 
 function App() {
   return (
-    <>
+    <Stack height={"100vh"}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Box flexGrow={1} >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
       <Footer />
-    </>
+    </Stack>
   );
 }
 
